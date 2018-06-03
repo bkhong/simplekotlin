19afb0ee88fd5a96ef76a03eb03aef4f0b983bb4
Brandon Hong <brandonhong@D-10-19-16-31.dhcp4.washington.edu>
2018-06-02 20:40:47 -0700

    Completed assignment

--- main.kts
+++ main.kts
@@ -3,14 +3,92 @@
 println("UW Homework: Simple Kotlin")
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
+fun whenFn(word: Any): String {
+    when(word) {
+        0 -> return("zero")
+        1 -> return ("one")
+        in 2..10 -> return("low number")
+        is Int -> return("a number")
+        "Hello" -> return("world")
+        is String -> return("Say what?")
+    }
+    return("I don't understand")
+}
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
+fun add(x: Int, y: Int): Int {
+    return(x + y)
+}
+
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+fun sub(x: Int, y: Int): Int {
+    return(x - y)
+}
+
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
+fun mathOp(x: Int, y: Int, op: (Int, Int) -> Int): Int {
+    return(op(x, y))
+}
 
 // write a class "Person" with first name, last name and age
+class Person(val firstName: String, val lastName: String, var age: Int) {
+    val debugString: String
+        get() = "[Person firstName:${firstName} lastName:${lastName} age:${age}]"
+
+    override fun hashCode(): Int {
+        return(firstName.hashCode() * lastName.hashCode() + age)
+    }
+
+    fun equals(other: Person): Boolean {
+        return(this.hashCode() == other.hashCode())
+    }
+}
 
 // write a class "Money"
+class Money(var amount: Int, var currency: String) {
+    init {
+        if (currency != "USD" && currency != "EUR" &&
+                currency != "CAN" && currency != "GBP") {
+            throw Exception("Currency not supported!")
+        } else if (amount < 0) {
+            throw Exception("Amount can't be less than zero!")
+        }
+    }
+
+    fun convert(newCurrency: String): Money {
+        var newAmount = 10 //default value for 10USD
+        when (this.currency) {
+            "USD" -> when (newCurrency) {
+                "GBP" -> newAmount = 5
+                "USD" -> newAmount = 10
+                "EUR" -> newAmount = 15
+                "CAN" -> newAmount = 15
+            }
+
+            "GBP" -> when (newCurrency) {
+                "GBP" -> newAmount = 5
+                "USD" -> newAmount = 10
+                "EUR" -> newAmount = 15
+            }
+
+            "EUR" -> when (newCurrency) {
+                "GBP" -> newAmount = 5
+                "USD" -> newAmount = 10
+                "EUR" -> newAmount = 15
+            }
+
+            "CAN" -> when(newCurrency) {
+                "USD" -> newAmount = 12
+            }
+        }
+        return Money(newAmount, newCurrency)
+    }
+
+    operator fun plus(other: Money): Money {
+        var changeMoney = other.convert(this.currency)
+        return(Money(changeMoney.amount + this.amount, this.currency))
+    }
+}
 
 // ============ DO NOT EDIT BELOW THIS LINE =============
 
